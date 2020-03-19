94ee3b0ffb0d4625875fa1bd9eb0c5d4be2edf59
blademainer <blademainer@gmail.com>
2020-03-19 17:53:34 +0800

    update mkdocs image

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v `pwd`:/docs -w /docs pjoc/mkdocs:v0.0.11 mkdocs build -d public
+docker run --rm -v `pwd`:/docs -w /docs pjoc/mkdocs:3d7cfb3 mkdocs build -d public
