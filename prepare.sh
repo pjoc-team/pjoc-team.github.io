c028bb5f7a22ac1cab159cd1fa8cc089e05f8cf3
blademainer <blademainer@gmail.com>
2020-03-20 23:55:30 +0800

    update theme

--- prepare.sh
+++ prepare.sh
@@ -4,14 +4,10 @@ set -x
 export NOW_YEAR="$(date +'%Y')"
 export SITE_URL="https://pjoc.pub"
 
-function gen_deploy_file() {
-	echo 'cat <<END_OF_TEXT' >  mkdocs_temp.sh
-	cat $1                 >> mkdocs_temp.sh
-	echo 'END_OF_TEXT'       >> mkdocs_temp.sh
-	bash mkdocs_temp.sh > "$2"
-	rm mkdocs_temp.sh
+echo 'cat <<END_OF_TEXT' >  mkdocs_temp.sh
+cat mkdocs_tpl.yml                 >> mkdocs_temp.sh
+echo 'END_OF_TEXT'       >> mkdocs_temp.sh
+bash mkdocs_temp.sh > "mkdocs.yml"
+rm mkdocs_temp.sh
 
-	cat $2
-}
-
-gen_deploy_file mkdocs_tpl.yml mkdocs.yml
+cat mkdocs.yml
