e5d25d3697e647a7958c79b9417354c948cd804a
blademainer <blademainer@gmail.com>
2020-10-01 20:00:33 +0800

    update docs

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -fr public
 sh prepare.sh
-docker run --rm -v "$(pwd)":/docs  -e LC_ALL=en_US.utf-8 -e LANG=en_US.utf-8 -e PYTHONIOENCODING=UTF-8 -w /docs pjoc/mkdocs:v0.0.14 mkdocs build -d public
+docker run --rm -v "$(pwd)":/docs  -w /docs pjoc/mkdocs:v0.0.15 mkdocs build -d public
